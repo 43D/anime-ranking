31d1e014e36a43d56c9b98ef72da3ff4f8cf48d3
Allan Fel <allan.goioere@gmail.com>
2022-10-28 16:19:50 -0300

    Test UI Player - Lista de reprodução hover

--- public/script/player/mediaManager.js
+++ public/script/player/mediaManager.js
@@ -35,9 +35,11 @@ export function mediaManager() {
         } else {
             const music = musicManagerClass.getMusicById(currentPlayList[Number(musicId)]);
             setVolume(config["volume"]);
-            
+            $(".list-group-item").removeClass("list-group-item-success");
+            $($("#list-play-ul").children().get(musicId)).addClass(" list-group-item-success");
             $("#title-head").html(music.name + " - " + music.artist);
             $("#name-bar").html(music.name + " - " + music.artist);
+
             switch (config["streaming"]) {
                 case "720":
                     if (music.urls.catbox["720"])
@@ -144,6 +146,9 @@ export function mediaManager() {
     }
 
     function createList() {
+        const config = localStorageObjectClass.getConfig();
+        const musicId = config["playnow"];
+
         $("#list-play-ul").empty();
         let i = 0;
         currentPlayList.forEach(function (k) {
@@ -151,6 +156,8 @@ export function mediaManager() {
             i++
         });
         eventsClass.skippedPlay();
+        $(".list-group-item").removeClass("list-group-item-success");
+        $($("#list-play-ul").children().get(musicId)).addClass(" list-group-item-success");
     }
 
     function makeIcon(icon) {
@@ -166,7 +173,7 @@ export function mediaManager() {
             btnPlay.append(makeIcon("bi bi-play"));
             div1.append(btnPlay);
 
-            let div2 = $("<div>").addClass("col-8 col-sm d-flex justify-content-start align-items-center").html(music.name);
+            let div2 = $("<div>").addClass("col-8 col-sm d-flex justify-content-start align-items-center").html(music.name + " - " + music.artist);
             let div3 = $("<div>").addClass("col-2 col-sm-1 d-flex align-items-center").html("00:00");
 
             row.append(div1);
